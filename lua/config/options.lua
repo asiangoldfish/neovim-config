ae640cfb014cdd20b3da5a3f2a5b9fc9b2e7b7e6
asiangoldfish <duongkhai@gmail.com>
2023-12-01 23:26:38 +0100

    Add red bar to indicate trailing white spaces

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,13 +2,25 @@
 
 local opt = vim.opt -- Macro
 
+-- Spaces
+-- Define highlight group ExtraWhitespace with red background
+vim.cmd("highlight ExtraWhitespace ctermbg=red guibg=red")
+-- Match trailing whitespaces on BufWinEnter
+vim.cmd("au BufWinEnter * match ExtraWhitespace /\\s\\+$/")
+-- Match trailing whitespaces on InsertEnter, excluding the cursor position
+vim.cmd("au InsertEnter * match ExtraWhitespace /\\s\\+\\%\\#\\@<!$/")
+-- Clear matches on InsertLeave
+vim.cmd("au InsertLeave * match ExtraWhitespace /\\s\\+$/")
+-- Clear matches on BufWinLeave
+vim.cmd("au BufWinLeave * call clearmatches()")
+
 -- Tab/Indentation
-opt.tabstop = 4 	-- How many spaces does tab character occupy
-opt.shiftwidth = 4	-- Set spaces for each indentation level
-opt.softtabstop = 4 	-- How many spaces is tab in Insert mode
-opt.expandtab = true	-- Convert tabs to spaces
-opt.smartindent = true	-- Allows auto indentation
-opt.wrap = wrap		-- Soft wrap (:set wrap to manually soft wrap)
+opt.tabstop = 4 -- How many spaces does tab character occupy
+opt.shiftwidth = 4 -- Set spaces for each indentation level
+opt.softtabstop = 4 -- How many spaces is tab in Insert mode
+opt.expandtab = true -- Convert tabs to spaces
+opt.smartindent = true -- Allows auto indentation
+opt.wrap = wrap -- Soft wrap (:set wrap to manually soft wrap)
 
 -- Search
 opt.incsearch = true
@@ -28,7 +40,7 @@ opt.completeopt = "menuone,noinsert,noselect"
 
 -- Behaviour
 opt.showmode = true
-opt.hidden = true	-- Change buffers without saving
+opt.hidden = true -- Change buffers without saving
 opt.errorbells = false
 opt.swapfile = false
 opt.backup = false
@@ -37,10 +49,11 @@ opt.undofile = true
 opt.backspace = "indent,eol,start"
 opt.splitright = true
 opt.splitbelow = true
-opt.autochdir = false	-- Automatically change the directory we're in
+opt.autochdir = false -- Automatically change the directory we're in
 opt.iskeyword:append("-")
 opt.mouse:append("a")
 opt.clipboard:append("unnamedplus")
 opt.modifiable = true
-opt.guicursor = "n-v-c:block,i-ci-ve:block,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blonkoff150-blinkon175"
+opt.guicursor =
+	"n-v-c:block,i-ci-ve:block,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blonkoff150-blinkon175"
 opt.encoding = "UTF-8"
