1c2112df9666c0ed97b282eb04f591736274ec3e
Khai Duong <duongkhai@gmail.com>
2024-02-27 10:27:08 +0100

    Split i3 config

---
+++ .config/i3/window_properties.sh
@@ -0,0 +1,9 @@
+# Window modes for applications
+for_window[class="Zotero"] floating enable
+for_window[class="Thunar"] floating enable
+for_window[class="peazip"] floating enable
+for_window[class="Yad"] floating enable
+
+# Enforce fibonacci layout
+default_orientation vertical #start in opposite orientation from your monitor
+for_window [class=".*"] split toggle
