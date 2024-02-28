5c4fc1e25d8c19809e619374e2bbd7bd7e080b73
Khai Duong <duongkhai@gmail.com>
2024-02-28 15:18:48 +0100

    Blueman is now a floating window

--- .config/i3/window_properties.sh
+++ .config/i3/window_properties.sh
@@ -3,6 +3,7 @@ for_window[class="Zotero"] floating enable
 for_window[class="Thunar"] floating enable
 for_window[class="peazip"] floating enable
 for_window[class="Yad"] floating enable
+for_window[class="Blueman-manager"] floating enable
 
 # Enforce fibonacci layout
 default_orientation vertical #start in opposite orientation from your monitor
