08cdbdbc30501752e032f6ebfa11dd2787e254ab
Khai Duong <khai@debian>
2024-03-09 22:11:36 +0100

    Improve Debian post installation
    - Add better comments and fix installation issues with nala
    - Add binary directory in ~/.local/bin
    - rustup skips path check to make re-installation automatic
    - Remove alacritty installation. This must now be done manually, although dependencies have been installed automatically.
    - Fix dotfiles directory path
    - Fix dotfiles deployment to the user's home directory
    - Add installion som binary from the internet for discord and golang (using wget)
    - Add installation via APT for ranger and polybar
    - Build and install lazygit
    - Remove prompt for reboot. This is bugged. The user has to manually reboot now- Add paths for '~/.local/bin', go bin directories for tooling and go applications

--- .config/bashrc/paths.sh
+++ .config/bashrc/paths.sh
@@ -12,3 +12,18 @@ fi
 if [ -d "/var/lib/flatpak/exports/bin" ]; then
     export PATH="$PATH:/var/lib/flatpak/exports/bin"
 fi
+
+# Binaries for the user only
+if [ -d ".local/bin" ]; then
+    export PATH="$PATH:~/.local/bin"
+fi
+
+# golang binary (ex. go toolchain)
+if [ -d ".local/bin/go/bin" ]; then
+    export PATH="$PATH:~/.local/bin/go/bin"
+fi
+
+# go application binaries (installed with 'go install')
+if [ -d "$HOME/go/bin" ]; then
+    export PATH="$PATH:$HOME/go/bin"
+fi
--- postinstall_scripts/debian.sh
+++ postinstall_scripts/debian.sh
@@ -10,25 +10,31 @@ set -x # Echo commands to make debugging easier.
 function install_essentials() {
     # apt frontend
     echo "Installing nala..."
-    sudo apt install nala -y 
-
-
-    
-    # For latex support...
-        # Look https://emacs.stackexchange.com/a/73197 for more info on
-        # rendering Latex on emacs
+    sudo apt install nala -y
 
     echo "Installing essentials..."
     sudo nala install -y \
-         vim git curl stow fish yad sxhkd cmake emacs feh picom ripgrep rofi \
-         dvipng texlive-latex-extra \
-    	 fonts-cantarell fonts-jetbrains-mono \
-         pandoc
+        vim git curl stow fish yad sxhkd cmake emacs feh picom ripgrep rofi \
+        ranger polybar
+
+    # For latex support...
+        # Look https://emacs.stackexchange.com/a/73197 for more info on
+        # rendering Latex on emacs
+    sudo nala install -y dvipna texlive-latex-extra
+
+    # Fonts
+    sudo nala install -y fonts-cantarell fonts-jetbrains-mono
+
+    # PDF converter
+    sudo nala install -y pandoc
+
+    # Binaries only for the user
+    mkdir -p ~/.local/bin
 }
 
 function install_rust() {
     echo "Installing rust..."
-    curl https://sh.rustup.rs -sSf | sh -s -- 
+    RUSTUP_INIT_SKIP_PATH_CHECK=yes curl https://sh.rustup.rs -sSf | sh -s -- 
 }
 
 function install_alacritty() {
@@ -38,14 +44,14 @@ function install_alacritty() {
         libxkbcommon-dev python3
     sudo nala install -y cargo
 
-    echo "Installing alacritty..."
+ #   echo "Installing alacritty..."
     # Get updated rust version
     # TODO: Fix this part. This tends to break for some reason (Incorrect rust version)
-    sleep 1
-    ~/.cargo/bin/rustup override set stable
-    ~/.cargo/bin/rustup update stable¨
-    alias rustc='~/.cargo/bin/rustc'
-    ~/.cargo/bin/cargo install alacritty
+#    sleep 1
+  #  ~/.cargo/bin/rustup override set stable
+   # ~/.cargo/bin/rustup update stable¨
+    #alias rustc='~/.cargo/bin/rustc'
+    #~/.cargo/bin/cargo install alacritty
 }
 
 function install_window_manager() {
@@ -57,20 +63,40 @@ function install_window_manager() {
 function install_dotfiles() {
     echo "Cloning neovim-config..."
     # Edit the URL if to use your own config
-    git clone --recurse-submodules https://github.com/asiangoldfish/neovim-config.git ~/config-stow
-    cd ~/config-stow
+    git clone --recurse-submodules https://github.com/asiangoldfish/neovim-config.git ~/dotfiles
+    cd ~/dotfiles
 
     # Overwrite existing files
-    echo "Deploying dotfiles from ~/config-stow"
-    stow --adopt .
-    git restore .
+    echo "Deploying dotfiles from ~/dotfiles"
+    stow --adopt
+    git restore --hard HEAD
+    stow .
 }
 
 function install_haskell() {
     # Installing Haskell toolchains
     # Source: https://stackoverflow.com/a/72953383
-    sudo nala install -y build-essential curl libffi-dev libffi8ubuntu1 libgmp-dev libgmp10 libncurses-dev libncurses5 libtinfo5 &&
+    sudo nala install -y build-essential curl libffi-dev libffi8ubuntu1 libgmp-dev libgmp10 libncurses-dev libncurses5 libtinfo5
     curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | BOOTSTRAP_HASKELL_NONINTERACTIVE=1 BOOTSTRAP_HASKELL_GHC_VERSION=latest BOOTSTRAP_HASKELL_CABAL_VERSION=latest BOOTSTRAP_HASKELL_INSTALL_STACK=1 BOOTSTRAP_HASKELL_INSTALL_HLS=1 BOOTSTRAP_HASKELL_ADJUST_BASHRC=P sh
+}
+
+function install_discord() {
+    mkdir -p ~/Downloads
+    cd ~/Downloads
+    wget "https://discord.com/api/download?platform=linux&format=deb" -O discord.deb
+    sudo nala install -y ./discord.deb
+    cd ~
+}
+
+function install_golang() {
+    cd ~/Downloads
+    wget 'https://go.dev/dl/go1.22.1.linux-amd64.tar.gz'
+    sudo tar -C ~/.local/bin -xzf '1.22.1.linux-amd64.tar.gz'
+    chmod u+x ~/.local/bin/go
+}
+
+function install_lazygit() {
+    go install github.com/jesseduffield/lazygit@latest
 }
 
 install_essentials
@@ -78,14 +104,18 @@ install_rust
 install_alacritty
 install_window_manager
 install_haskell
+install_discord
+install_golang
+install_lazygit
+
+set +x
+
+# Message to install alacritty
+echo "To install alacritty, open a new terminal and run the following command:"
+echo "~/.cargo/bin/cargo install alacritty"
+echo ""
 
 # Prompt for reboot
-read -r -p "Post installation successfully completed!
-It's recommended to reboot. Continue? [Y/n] " response
-case "$response" in
-    [nN][oO]) 
-        break
-        ;;
-    *)
-        systemctl reboot
-esac
+echo "Post installation successfully completed!"
+echo "It's recommended to reboot with 'systemctl reboot'"
+
