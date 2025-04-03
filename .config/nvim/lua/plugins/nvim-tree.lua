77a96b9fab4544f76f2799b16d65dc1b6c8b0930
Khai Duong <duongkhai@gmail.com>
2025-04-03 17:25:30 +0200

    [Neovim] Improve telescope UI for file finder and livegrep

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -4,6 +4,9 @@ return {
     opts = {
         filters = {
             dotfiles = false
+        },
+        update_focused_file = {
+            enable = true,
         }
     }
 }
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -9,25 +9,72 @@ local config = function()
                     ["<C-j>"] = "move_selection_next",
                     ["<C-k>"] = "move_selection_previous",
                 }
-            }
+            },
         },
         pickers = {
             find_files = {
                 theme = "dropdown",
-                previewer = false,
-                hidden = true
+                previewer = true,
+                hidden = true,
+                layout_strategy = "horizontal",
+                layout_config = {
+                    horizontal = {
+                        prompt_position = "top",
+                        width = 0.8,
+                        height = 0.7,
+                        preview_width = 0.5,
+                    }
+                },
             },
             live_grep = {
                 theme = "dropdown",
-                previewer = true
+                previewer = true,
+
+                layout_strategy = "horizontal",
+                layout_config = {
+                    horizontal = {
+                        prompt_position = "top",
+                        width = 0.8,
+                        height = 0.7,
+                        preview_width = 0.5,
+                    }
+                },
+                --mappings = {
+                --    i = {
+                --        ["C-Up"] = "preview_scrolling_up",
+                --        ["C-Down"] = require("telescope.actions").preview_scrolling_down,
+                --    },
+                --    n = {
+                --        ["C-Up"] = "preview_scrolling_up",
+                --        ["C-Down"] = require("telescope.actions").preview_scrolling_down,
+                --    },
+                --}
             },
             find_buffers = {
                 theme = "dropdown",
-                previewer = false
+                previewer = false,
+                layout_strategy = "horizontal",
+                layout_config = {
+                    horizontal = {
+                        prompt_position = "top",
+                        width = 0.8,
+                        height = 0.7,
+                        preview_width = 0.5,
+                    }
+                },
             },
             projects = {
                 theme = "dropdown",
-                previewer = "false"
+                previewer = "false",
+                layout_strategy = "horizontal",
+                layout_config = {
+                    horizontal = {
+                        prompt_position = "top",
+                        width = 0.8,
+                        height = 0.7,
+                        preview_width = 0.5,
+                    }
+                },
             }
         }
     })
@@ -47,6 +94,6 @@ return {
         keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>"),
         keymap.set("n", "<leader>ff", ":Telescope find_files<CR>"),
         keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>"),
-        keymap.set("n", "<leader>fb", ":Telescope buffers<CR>")
+        keymap.set("n", "<leader>fb", ":Telescope buffers<CR>"),
     }
 }
