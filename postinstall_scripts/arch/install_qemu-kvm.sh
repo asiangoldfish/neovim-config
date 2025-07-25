9458034cf284f484572e172b0140c1451f41a527
Khai Duong <duongkhai@gmail.com>
2025-07-25 12:51:21 +0200

    [Arch Install] Update virt-manager installation

--- postinstall_scripts/arch/install_qemu-kvm.sh
+++ postinstall_scripts/arch/install_qemu-kvm.sh
@@ -1,5 +1,7 @@
-sudo systemctl enable --now libvirtd.socket
-sudo usermod -aG libvirt $USER
-sudo modprobe kvm
-sudo usermod -aG kvm $USER
+sudo pacman -S qemu virt-manager dnsmasq virt-viewer vde2 bridge-utils libguestfs
 
+sudo systemctl enable --now libvirtd
+sudo usermod -aG libvirt $USER
+sudo systemctl restart libvirtd
+# sudo modprobe kvm
+# sudo usermod -aG kvm $USER
